164fb6beb74e8aa429128271253cc48f5ce373ad
Huzaifa Akhtar <huzaifaakh@outlook.com>
2026-07-22 10:53:05 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function changeHandler(inputId, newValue) {
+    setUserInput((previousUserInput) => {
+      return {
+        ...previousUserInput,
+        [inputId]: newValue,
+      };
+    });
+  }
+  return (
+    <>
+      <Header />
+      <UserInput onChange={changeHandler} userInput={userInput} />
+      <Results input={userInput}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Results.jsx
@@ -0,0 +1,5 @@
+function Results({ input }) {
+  return <div id="results"></div>;
+}
+
+export default Results;
