9489c04de391eb074715c7d5594c03d4a3e625b8
Huzaifa Akhtar <huzaifaakh@outlook.com>
2026-07-23 06:52:26 -0700

    file updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import UserInput from "./components/UserInput";
 import Results from "./components/Results";
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
+    initialInvestment: 15000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
+  const inputValidity = userInput.duration >= 1;
   function changeHandler(inputId, newValue) {
     setUserInput((previousUserInput) => {
       return {
@@ -22,7 +23,8 @@ function App() {
     <>
       <Header />
       <UserInput onChange={changeHandler} userInput={userInput} />
-      <Results input={userInput}/>
+      {inputValidity ? <Results input={userInput} /> : <div className="center">Please enter a valid duration (greater than 0)</div>}
+      
     </>
   );
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,37 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 function Results({ input }) {
   const result = calculateInvestmentResults(input);
-  console.log(result)
-  return <div id="results"></div>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest </th>
+          <th>Interested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {result.map((item) => {
+          const totalInterest =
+            item.valueEndOfYear -
+            item.annualInvestment * item.year -
+            input.initialInvestment;
+          const totalAmountInvested = item.valueEndOfYear - totalInterest
+          return (
+            <tr key={item.year}>
+              <td>{item.year}</td>
+              <td>{formatter.format(item.valueEndOfYear)}</td>
+              <td>{formatter.format(item.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Results;
